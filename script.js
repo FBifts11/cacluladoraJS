099888e2ec0c7841e856316e2fa86bf3f64f7fc7
FBifts11 <federico.balbuena@bue.edu.ar>
2024-08-17 21:21:44 -0300

    Se agrega funcionalidad de encendido/apagado

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
+let onOff = false;
+allowedEntry = true;
+
+function encenderApagar() {
+    let buttons = document.getElementsByClassName('btn');
+
+    if (onOff == false) {
+        document.getElementById('display').style.backgroundColor = "yellowgreen";
+        document.getElementById('onOff').value = 'Off';
+        document.getElementById('onOff').style.backgroundColor = 'orange';
+        setTimeout(() => {
+            document.getElementById('display').value = "0";
+
+        }, 500);
+        onOff = true;
+        allowedEntry = true;
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = false;
+        };
+
+    } else {
+        
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+        };
+
+        document.getElementById('display').value = ""
+        document.getElementById('display').style.backgroundColor = "gray";
+        document.getElementById('onOff').value = 'On';
+        document.getElementById('onOff').style.backgroundColor = 'green';
+        onOff = false;
+        allowedEntry = false;
+    }
+
+}
+
 function ingresarDato(valor) {
-    document.getElementById("display").value += valor;
+    if (allowedEntry == false) {
+        document.getElementById('display').value = '';
+    } else {
+
+        if (document.getElementById('display').value == "0") {
+            document.getElementById("display").value = '';
+        }
+        document.getElementById("display").value += valor;
+    }
 }
 
 function borrar() {
-    document.getElementById("display").value = "";
+    if (allowedEntry == false) {
+        document.getElementById("display").value = "";
+    } else {
+
+        document.getElementById("display").value = "0";
+    }
 }
 
 function calcular() {
-    const valorDisplay = document.getElementById('display').value;
+    if (allowedEntry == false) {
+        document.getElementById("display").value = "";
+    } else {
+        const valorDisplay = document.getElementById('display').value;
     try {
         const resultado = eval(valorDisplay);
         console.log(resultado);
@@ -16,4 +68,6 @@ function calcular() {
         console.log("Error: " + error.message);
         document.getElementById('display').value = "Error";
     }
+    }
 }
+
